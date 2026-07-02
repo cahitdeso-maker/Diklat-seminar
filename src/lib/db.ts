85596a9d3141f129acac2f3a69decfb2e7401511
cahitdeso-maker <cahitdeso@gmail.com>
2026-07-02 15:26:18 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,13 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const poolConnection = mysql.createPool({
+const client = postgres({
   host: "aws-1-ap-southeast-1.pooler.supabase.com",
   port: 5432,
-  user: "postgres.bdzqkxscwgdbnneuhjjh",
-  password: "pkugombong21",
   database: "postgres",
-  waitForConnections: true,
-  connectionLimit: 10,
+  username: "postgres.bdzqkxscwgdbnneuhjjh",
+  password: "YOUR_PASSWORD",
+  ssl: "require",
 });
 
 export const db = drizzle(client, { schema });
