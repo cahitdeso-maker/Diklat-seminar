e9a7ae775274333b8de6dc3344d4202895753f28
Mahmudd <mahmuddinudin00@gmail.com>
2026-07-16 06:22:35 +0700

    Update aplikasi

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,11 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 const client = postgres({
-  host: "aws-1-ap-southeast-1.pooler.supabase.com",
+  host: "postgresql://neondb_owner:npg_6BfzbFru0cad@ep-odd-forest-aobh0k79-pooler.c-2.ap-southeast-1.aws.neon.tech/db-seminar?sslmode=require&channel_binding=require",
   port: 5432,
-  database: "postgres",
-  username: "postgres.bdzqkxscwgdbnneuhjjh",
-  password: "pkugombong21",
+  database: "db-seminar",
+  username: "neondb_owner",
+  password: "npg_6BfzbFru0cad",
   ssl: "require",
 });
 
